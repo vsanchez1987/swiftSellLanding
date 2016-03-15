29aa2310543ac86e75ccebf33502dc8876b8e49d
victor sanchez <vsanchez1987@yahoo.com>
2016-03-14 23:00:39 -0700

    pushing the landing page for the first time

---
+++ js/vendor.js
@@ -0,0 +1,10 @@
+"use strict";
+$(document).ready(function() {
+  $(".caption > span").typed({
+    strings: ["Video chat^1000", "Message chat^1000", "Same day delivery^1000"],
+    typeSpeed: 100,
+    loop: 1,
+    loopCount: 4,
+    BackDelay: 2500
+  })
+});
